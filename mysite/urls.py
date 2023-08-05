10e962e3b9edb6f214ce6c039fb87910bf5534f9
Ильми Джепаров <jilmi@yandex.ru>
2023-08-05 18:49:00 +0200

    lesson_2.1

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-
-    path('index/', include('shopapp.urls')),
+    path("admin/", admin.site.urls),
+    path("index/", include("shopapp.urls")),
 ]
