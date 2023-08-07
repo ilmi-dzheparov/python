77c7c64bd612062ff33db36a8eb2d3d77956b3c5
Ильми Джепаров <jilmi@yandex.ru>
2023-08-07 15:20:38 +0200

    lesson 4.2

--- shopapp/admin.py
+++ shopapp/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
-from shopapp.models import Product
+from shopapp.models import Product, Order
 
+
+class OrderInline(admin.TabularInline):
+    model = Product.orders.through
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
@@ -9,11 +12,48 @@ class ProductAdmin(admin.ModelAdmin):
     list_display_links = "pk", "name"
     ordering = "pk", "name"
     search_fields = "name", "description"
+    inlines = [
+        OrderInline,
+    ]
+    fieldsets = [
+        (None, {
+            "fields": ("name", "description")
+        }),
+        ("Price options", {
+            "fields": ("price", "discount"),
+            "classes": ("wide", "collapse"),
+        }),
+        ("Extra options", {
+            "fields": ("archived",),
+            "classes": ("collapse",),
+            "description": "Extra options. Field 'archived' is for soft delete",
+        }),
+    ]
+
 
     def description_short(self, obj: Product) -> str:
         if len(obj.description) < 48:
             return obj.description
         return obj.description[:48] + '...'
 
-
 # admin.site.register(Product, ProductAdmin)
+
+class ProductInline(admin.StackedInline):
+    model = Order.products.through
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = "delivery_address", "promocode", "created_at", "user_verbose"
+    inlines = [
+        ProductInline,
+    ]
+    # list_display_links = "pk", "name"
+    # ordering = "pk", "name"
+    # search_fields = "name", "description"
+
+    def get_queryset(self, request):
+        return Order.objects.select_related("user").prefetch_related("products")
+
+    def user_verbose(self, obj: Order) -> str:
+        return obj.user.first_name or obj.user.name
