1a3773518273db887c83d283e3bc9137822fe012
Ильми Джепаров <jilmi@yandex.ru>
2023-08-16 14:52:15 +0200

    lesson 9.1

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "shopapp.apps.ShopappConfig",
-    "requestdataapp.apps.RequestdataappConfig"
+    "requestdataapp.apps.RequestdataappConfig",
+    "myauth.apps.MyauthConfig",
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_REDIRECT_URL = "/admin/"
