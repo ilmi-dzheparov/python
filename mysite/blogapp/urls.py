7697a2db0679d53ba327e61433b8ea94a018612a
Ильми Джепаров <jilmi@yandex.ru>
2023-09-07 20:10:14 +0200

    lesson 18.2

--- mysite/blogapp/urls.py
+++ mysite/blogapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 
-from .views import ArticlesListView
+from .views import ArticlesListView, ArticleDetailView, LatestArticlesFeed
 
 
 app_name = "blogapp"
 urlpatterns = [
     path("articles/", ArticlesListView.as_view(), name="articles"),
+    path("articles/<int:pk>/", ArticleDetailView.as_view(), name="article"),
+    path("articles/latest/feed/", LatestArticlesFeed(), name="articles-feed"),
 ]
--- mysite/blogapp/views.py
+++ mysite/blogapp/views.py
@@ -1,16 +1,46 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from blogapp.models import Article
+
+from django.contrib.syndication.views import Feed
+from django.urls import reverse_lazy, reverse
 
 
 class ArticlesListView(ListView):
     template_name = "shopapp/article_list.html"
     # model = Article
     queryset = (Article.objects
-                .defer("content")
+                # .defer("content")
                 .select_related("author")
                 .select_related("category")
                 .prefetch_related("tags")
-                .order_by('pk'))
+                .filter(pub_date__isnull=False)
+                .order_by('pub_date')
+                )
     context_object_name = "articles"
 
+
+class ArticleDetailView(DetailView):
+    model = Article
+
+
+class LatestArticlesFeed(Feed):
+    title = "Blog articles (latest)"
+    description = "Updates on changes addition blog articles"
+    link = reverse_lazy("blogapp:articles")
+
+    def items(self):
+        return(
+            Article.objects
+            .filter(pub_date__isnull=False)
+            .order_by("-pub_date")[:5]
+        )
+
+    def item_title(self, item: Article):
+        return item.title
+
+    def item_description(self, item: Article):
+        return item.content[:200]
+
+    def item_link(self, item: Article):
+        return reverse("blogapp:article", kwargs={"pk": item.pk})
